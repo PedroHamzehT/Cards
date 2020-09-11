d87f9196fb50ce8980359c4b4bc5dc08577d3e3f
pedro@9jobs <pedro@99jobs.com>
2020-09-10 21:50:03 -0300

    Add the contains method

--- lib/cards.ex
+++ lib/cards.ex
@@ -2,4 +2,12 @@ defmodule Cards do
   def create_deck do
     ["Ace", "Two", "Three"]
   end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, hand) do
+    Enum.member?(deck, hand)
+  end
 end
